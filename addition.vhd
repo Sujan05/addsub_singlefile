5a87842585728d3082af00e4a2ac7104c6109990
Sujan Saha <sujansaha@Sujans-MacBook-Air.local>
2018-06-07 19:12:21 -0500

    addition modified

--- addition.vhd
+++ addition.vhd
@@ -1,28 +1,29 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company:
+-- Engineer:
+--
 -- Create Date: 06/02/2018 01:38:52 PM
--- Design Name: 
+-- Design Name:
 -- Module Name: addition - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
+use IEEE.STD_LOGIC_ARITH.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -57,6 +58,8 @@ end Behavioral;
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 
 entity subtraction is
   Port (a, b: in STD_LOGIC_VECTOR(3 downto 0);
@@ -71,6 +74,7 @@ end Behavioral;
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
 
 entity addsub is
   Port (a, b: in STD_LOGIC_VECTOR(3 downto 0);
@@ -79,24 +83,31 @@ entity addsub is
 end addsub;
 
 architecture Behavioral of addsub is
-    signal add_result, sub_result: STD_LOGIC_VECTOR(3 downto 0);
-    component addition 
-        port(a, b: in STD_LOGIC_VECTOR(3 downto 0);
+
+    component addition
+        port(a: in STD_LOGIC_VECTOR(3 downto 0);
+             b: in STD_LOGIC_VECTOR(3 downto 0);
              output: out STD_LOGIC_VECTOR(3 downto 0) );
     end component;
-    component subtraction 
-            port(a, b: in STD_LOGIC_VECTOR(3 downto 0);
+    component subtraction
+            port(a: in STD_LOGIC_VECTOR(3 downto 0);
+                 b: in STD_LOGIC_VECTOR(3 downto 0);
                  output: out STD_LOGIC_VECTOR(3 downto 0) );
     end component;
+    signal add_result: STD_LOGIC_VECTOR(3 downto 0);
+    signal sub_result: STD_LOGIC_VECTOR(3 downto 0);
 begin
-    add: addition port map(a, b, add_result);
-    sub: subtraction port map(a, b, sub_result);
-    p1: process(sel)
-        begin
+    add: addition port map(a=>a, b=>b, output=>add_result);
+    sub: subtraction port map(a=>a, b=>b, output=>sub_result);
+    p1: process(add_result, sub_result, sel)
+    begin
             if sel = '1' then
               output <= add_result;
-            else
+            else if sel = '0' then
               output <= sub_result;
+            else output <= "XXXX";
             end if;
-        end process;
+          end if;
+        --end;
+       end process;
 end Behavioral;
